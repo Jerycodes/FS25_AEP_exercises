5b672630c71bdded4b8ad64472ddba999fe4ff29
Jeremy Nathan <jeremyevrjr.nathan@gmail.com>
2025-03-14 11:40:34 +0100

    added more exercises of week 2 and restructured the solutions together with ChatGPT. Added a legend to the solutions with important functions and more.

--- exercises.py
+++ exercises.py
@@ -54,3 +54,81 @@ def bmi_calculation():
     print(bmi)
 
 bmi_calculation()
+
+# exercises week 2: Python
+
+# 1.
+number_1 = 224
+number_2 = '435.5'
+number_3 = 435.5
+
+# 2.
+print(type(number_1))
+print(type(number_2))
+print(type(number_3))
+
+# 3.
+number_1 = str(number_1)
+number_2 = float(number_2)
+number_3 = int(number_3)
+
+print(type(number_1))
+print(type(number_2))
+print(type(number_3))
+
+print(number_1)
+print(number_2)
+print(number_3)
+
+# 4. -
+
+# 5.
+var_1 = ["This is my ", "list number", 1]
+var_2 = ("This is my ", "tuple number", 1)
+var_3 = {"dict_numb" : 1, "content": "this is my first dictionary"}
+var_4 = {"this is my ", "set number", 1}
+
+print(type(var_1))
+print(type(var_2))
+print(type(var_3))
+print(type(var_4))
+
+# 7.
+list_1 = ["liste", 455, "another string"]
+print(list_1[1])
+
+# 8.
+#a)
+list_1.append(66)
+
+#b)
+list_2 = ["hello", 312]
+list_1.append(list_2)
+
+#c)
+print(list_1)
+
+#d)
+print(list_1[4][1])
+
+#e)
+list_3 = ["another", "list"]
+list_1.extend(list_3)
+
+#f)
+print(list_1[-2])
+
+# 9.
+#a)
+list_numbers = [1,2,3,4,5,6,7,8,9,10]
+for number in list_numbers:
+    print(number)
+
+for number in range(1, 11):
+    print(number)
+
+#b)
+z = 1
+while z < 11:
+    print(z)
+    z += 1
--- solutions.py
+++ solutions.py
@@ -1,35 +1,61 @@
-# 1) Gib folgendes Statement aus: “\” wird «Backslash» genannt
-# 2) Erstelle folgende Variable: hotel_name = "Adler"
-# Gib den Datentyp aus (Tipp: type())
-# 3) Schreibe ein Script, welches eine Zahl als Input vom User verlangt. Das Programm soll
-# danach die Zahl ausgeben. (Tipp: input())
-# 4) Schreibe ein Script, dass den User auffordert einen Wochentag einzugeben. Danach
-# soll folgendes ausgegeben werden:
-# a. für die Wochentage Montag bis Freitag: Arbeitstag
-# b. für die Wochentage Samstag und Sonntag: Wochenende
-# Tipp: if/else/elif
-# 5) Schreibe eine Funktion, die den BMI berechnet und retourniert, basierend auf dem
-# Gewicht und der Körpergrösse. Folgendes ist die Funktion: BMI = Gewicht in kg /
-# (Körpergrösse in m)2
-# Tipp 1: die Funktion muss zwei Parameter haben, Gewicht und Körpergrösse
-# Tipp 2: eine Zahl zu quadrieren geht mit «**»
-# 6) Schreibe eine Funktion, in welcher ein User zuerst das Gewicht und danach die
-# Körpergrösse eingeben muss. Speichere die Eingabe in jeweiligen Variablen.
-# 7) Füge die beiden Funktionen von Nr. 6) und Nr. 7) zu einer neuen Funktion
-# zusammen. Tipp: Falls es eine Fehlermeldung gibt, musst du allenfalls die Datentypen
-# explizit zuweisen. Du kannst das machen mit z.B. folgenden Funktionen: int(), float(),
-# str().
+"""
+# Lösungen Woche 1 & 2
+
+## Legende: Wichtige Python-Funktionen
+
+### Listen-Methoden
+- `.append(x)`: Fügt `x` am Ende der Liste hinzu.
+- `.extend(iterable)`: Fügt Elemente aus `iterable` zur Liste hinzu (ohne Verschachtelung).
+- `.insert(i, x)`: Fügt `x` an Index `i` in die Liste ein.
+- `.remove(x)`: Entfernt das erste Vorkommen von `x` in der Liste.
+- `.pop(i)`: Entfernt und gibt das Element an Index `i` zurück (Standard: letztes Element).
+- `.index(x)`: Gibt den Index des ersten Vorkommens von `x` zurück.
+- `.count(x)`: Zählt, wie oft `x` in der Liste vorkommt.
+- `.sort()`: Sortiert die Liste in aufsteigender Reihenfolge.
+- `.reverse()`: Kehrt die Reihenfolge der Liste um.
+
+### String-Methoden
+- `.strip()`: Entfernt Leerzeichen oder bestimmte Zeichen am Anfang und Ende des Strings.
+- `.lower()`, `.upper()`: Wandelt den String in Klein- bzw. Großbuchstaben um.
+- `.replace(a, b)`: Ersetzt `a` durch `b` im String.
+- `.split(sep)`: Teilt den String an `sep` und gibt eine Liste zurück.
+- `.join(iterable)`: Verbindet Elemente aus `iterable` mit dem String als Trennzeichen.
+
+### Dictionary-Methoden
+- `.keys()`: Gibt eine Liste der Schlüssel zurück.
+- `.values()`: Gibt eine Liste der Werte zurück.
+- `.items()`: Gibt eine Liste von Schlüssel-Wert-Tupeln zurück.
+- `.get(key, default)`: Gibt den Wert zu `key` zurück oder `default`, falls `key` nicht existiert.
+- `.pop(key)`: Entfernt den Schlüssel `key` und gibt den Wert zurück.
+
+### Wichtige Funktionen
+- `type(x)`: Gibt den Datentyp von `x` zurück.
+- `int(x)`, `float(x)`, `str(x)`: Wandelt `x` in einen anderen Datentyp um.
+- `range(start, stop, step)`: Erstellt eine Zahlenreihe von `start` bis `stop-1` mit `step`-Schritten.
+- `input(prompt)`: Fragt den Benutzer nach einer Eingabe.
+- `len(x)`: Gibt die Länge von `x` zurück (z. B. Anzahl der Elemente in einer Liste).
+
+Weitere Infos:
+- [Python Listen-Methoden](https://www.python-kurs.eu/python3_listen.php)
+- [Python Datentypen](https://docs.python.org/3/tutorial/datastructures.html)
+- [Python Collections Vergleich](https://www.educative.io/answers/list-vs-tuple-vs-set-vs-dictionary-in-python)
+
+"""
+
+# -------------------------------
+# Lösungen Woche 1
+# -------------------------------
 
 # 1) Backslash ausgeben
-print("\"\\\" wird 'Backslash' genannt")
+print('"\\" wird "Backslash" genannt')
 
 # 2) Typ einer Variable ausgeben
-hotelname = "Adler"
-print(type(hotelname))
+hotel_name = "Adler"
+print(type(hotel_name))
 
 # 3) Benutzerinput als Zahl
-number = input("Gib eine beliebige Zahl ein: ")
-print(number)
+type_number = input("Gib eine beliebige Zahl ein: ")
+print(type_number)
 
 # 4) Wochentag abfragen und klassifizieren
 weekday = input("Gib einen Wochentag an: ")
@@ -57,11 +83,11 @@ def bmi_berechner(gewicht, körpergrösse):
 
 print(bmi_berechner(74, 1.87))
 
-# 6) Eingabe von Gewicht und Größe als Funktion
+# 6) Eingabe von Gewicht und Körpergrösse
 def bmi_input():
     gewicht = input("Gib dein Gewicht ein in kg: ")
     körpergrösse = input("Gib deine Körpergrösse ein in m: ")
-    return (gewicht, körpergrösse)
+    return gewicht, körpergrösse
 
 bmi_input()
 
@@ -76,60 +102,21 @@ def bmi_rechner():
 
 bmi_rechner()
 
-# Aufgaben:
-# 1) Erstelle folgende Variablen:
-# a. number_1 = 224
-# b. number_2 = '435.5'
-# c. number_3 = 435.5
-# 2) Gib bei allen nummern den Typ aus
-# 3) Wechsle:
-# a. number_1 in einen string
-# b. number_2 in einen float
-# c. number_3 in einen integer
-# und gib erneut den Datentyp aus. Was geschah mit number_3?
-# 4) Weshalb ist das wichtig?
-# 5) Erstelle folgende Variablen und gib für alle den Datentyp aus.
-# a. var_1 = ["this is my ", "list number", 1]
-# b. var_2 = ("this is my ", "tuple number", 1)
-# c. var_3 = {"dict_numb " : 1, "content": "this is my first dictionary"}
-# d. var_4 = {"this is my ", "set number", 1}
-# 6) lies die kurze Zusammenfassung über die verschiedenen python collections nach:
-# https://www.educative.io/answers/list-vs-tuple-vs-set-vs-dictionary-in-python.
-# Detailliertere Informationen findet ihr beispielsweise hier:
-# https://docs.python.org/3/tutorial/datastructures.html
-# a.
-# 7) 8) 9) a. b. c. d. e. Erstelle eine Liste (list_1) mit folgenden 3 Werten: «liste», 455, «another string».
-# Gebe den zweiten Wert (455) aus via Index. Recap Woche 1: Welche Nummer wird
-# gebraucht für den zweiten Wert?
-# Schau dir die methoden von Listen an: https://www.python-
-# kurs.eu/python3_listen.php
-# Füge den integer «66» zur list_1 hinzu
-# Füge eine liste (list_2 = ["hello", 312]) zur list_1 hinzu
-# Was ist das Resultat von b?
-# Gebe nun den integer «312» der list_2 aus. Benutze dazu die list_1 und
-# Indexierung.
-# Füge die Elemente der folgenden Liste (list_3 = ["another", "list"]) der list_1
-# hinzu. Beachte: wir wollen hier keine weitere Verschachtelung, wir wollen nur
-# die Elemente hinzufügen!
-# f. Gebe den string «another» aus, indem du negative indexierung benutzt
-# Wir haben die Zahlen von 1 bis (und mit) 10. Gebe die Zahlen untereinander aus,
-# indem du:
-# a. Dafür einen «for-loop» programmierst
-# b. Dafür einen «while-loop» programmierst
-# Tipp: Benutze für die Funktion von range(), oder mache eine Liste mit allen
-# Nummern von 1 bis 10.
+# -------------------------------
+# Lösungen Woche 2
+# -------------------------------
 
-# 1)
+# 1) Variablen definieren
 number_1 = 224
 number_2 = '435.5'
 number_3 = 435.5
 
-# 2)
+# 2) Datentypen ausgeben
 print(type(number_1))
 print(type(number_2))
 print(type(number_3))
 
-# 3)
+# 3) Datentypen konvertieren
 number_1 = str(number_1)
 number_2 = float(number_2)
 number_3 = int(number_3)
@@ -138,78 +125,53 @@ print(type(number_2))
 print(type(number_3))
 print(number_3)
 
-# 4) Wenn man z.b. mathematische funktionen erstellt, kann es zu einem
-# «Type error» kommen, da wir allenfalls im Hintergrund einen string
-# mit einem integer versuchen zu multiplizieren. Deshalb ist es immer
-# wichtig zu überprüfen, welche datentypen man vor sich hat und wo man
-# allenfalls eine datenkonvertierung braucht. Man kann auch bei
-# Funktionen dafür vorprogrammieren, welcher Datentyp der Argumente
-# erwartet wird.
+# 4) Warum ist das wichtig?
+# Datentypen müssen korrekt sein, um Fehler in Berechnungen zu vermeiden.
 
-# 5)
+# 5) Verschiedene Datentypen und ihre Typen
 var_1 = ["this is my ", "list number", 1]
 var_2 = ("this is my ", "tuple number", 1)
-var_3 = {"dict_numb " : 1, "content": "this is my first dictionary"}
+var_3 = {"dict_numb": 1, "content": "this is my first dictionary"}
 var_4 = {"this is my ", "set number", 1}
 print(type(var_1))
 print(type(var_2))
 print(type(var_3))
 print(type(var_4))
 
-# 6) N.a.
+# 6) N.A.
 
-# 7)
+# 7) Listenoperationen
 list_1 = ["liste", 455, "another string"]
 print(list_1[1])
 
-# Da der Computer bei «0» mit dem Zählen beginnt, müssen wir für den
-# Index des zweiten Wertes die Nummer «1» benutzen. Eine Übersicht gibt
-# es hier:
-
-# 8) Aufgaben:
-# a.
+# 8a) Integer zur Liste hinzufügen
 list_1.append(66)
 
-# b.
+# 8b) Liste zu einer Liste hinzufügen
 print(list_1)
 list_2 = ["hello", 312]
 list_1.append(list_2)
 print(list_1)
 
-# c.
-# Wir erhalten eine “verschachtelte” (engl: “nested») Liste, also
-# eine Liste die eine andere Liste enthält.
+# 8c) Nested List erhalten
 
-# d.
+# 8d) Zugriff auf verschachtelte Liste
 print(list_1[4][1])
 
-# was geschieht hier? Die Liste «list_1» sieht folgendermassen
-# aus: ['liste', 455, 'another string', 66, ['hello', 312]]. wir
-# gehen nun auf den Index «4», welcher wiederum eine Liste
-# enthält (['hello', 312]). Wenn wir nur das print statement
-# print(list_1[4]) ausgeben, geben wir die gesamte Liste aus. Wir
-# wollen aber wiederum nur das zweite Element (Index: 1)
-# ausgeben. Deshalb «verketten» wir beide Indexe miteinander:
-# print(list_1[4][1]).
-
-# e.
+# 8e) Elemente der Liste hinzufügen
 list_3 = ["another", "list"]
 list_1.extend(list_3)
 print(list_1)
 
-# f.
+# 8f) Negativer Index für 'another'
 print(list_1[-2])
 
-# 9)
-print("+++++++++++++++++++++ \n for-loop mit Liste:")
-numbers_1_10 = [1,2,3,4,5,6,7,8,9,10]
-for i in numbers_1_10:
-print(i)
-print("+++++++++++++++++++++ \n for-loop mit range()")
-for q in range(1,11):
-print(q)
-print("+++++++++++++++++++++ \n while loop mit range()")
+# 9a) Zahlen mit for-Loop ausgeben
+for i in range(1, 11):
+    print(i)
+
+# 9b) Zahlen mit while-Loop ausgeben
 z = 1
-while z < 11:
-print(z)
-z += 1
+while z <= 10:
+    print(z)
+    z += 1
